a4fb28502cc3c890727e3990c03d562e4fbdbb2e
Tin Oreskovic <34209614+Tin97@users.noreply.github.com>
2018-09-30 23:30:07 +0100

    Added comments

--- Assignment1/lca.cpp
+++ Assignment1/lca.cpp
@@ -17,20 +17,25 @@ struct node
 class btree
 {
     public:
+        //when tree object created sets root to NULL
         btree()
         {
           root = NULL;
         }
 
+        //public lowest common ancestor function, calls the pricate function
         int lca(int n1, int n2)
         {
           return findLCA(root, n1, n2);
         }
 
+        //insert the node with the value key
         void insert(int key)
         {
+            //if the tree is not empty calls the recursive insert function
             if(root!=NULL)
               insert(key, root);
+            //if the tree is empty set the key value to be the given key
             else
             {
               root=new node;
@@ -41,20 +46,28 @@ class btree
         }
 
     private:
+      //returns the key value of the lowest common ancestor
       int findLCA(node *root, int n1, int n2)
       {
           vector<int> path1, path2;
 
+          //first checks whether the keys exist and if they do stores
+          //key values (path) in vector objects
           if ( !findPath(root, path1, n1) || !findPath(root, path2, n2))
                 return -1;
 
           int i;
+
+          //compares values from two different paths, when they are not
+          //the same for loop breakes and returns the last same key
+          //from the path
           for (i = 0; i < path1.size() && i < path2.size() ; i++)
               if (path1[i] != path2[i])
                   break;
           return path1[i-1];
       }
 
+      //find the path for a specific node from the root node
       bool findPath(node *root, vector<int> &path, int k)
       {
           if (root == NULL) return false;
@@ -72,6 +85,7 @@ class btree
           return false;
       }
 
+      //insert the node with the key value recursively
       void insert(int key, node *leaf)
       {
           if(key< leaf->key)
@@ -103,6 +117,7 @@ class btree
         node *root;
 };
 
+//catch2 unit testing
 TEST_CASE( "Lowest common ancestor", "[lca]" ) {
     btree tree = btree();
     tree.insert(7);
