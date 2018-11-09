310ab4d4c5721e49b8d3f21adc7b5a9658e1f72c
Tin Oreskovic <34209614+Tin97@users.noreply.github.com>
2018-11-09 21:47:44 +0000

    added am option for user to select a repository and see information about it

--- Github Access/Access.py
+++ Github Access/Access.py
@@ -1,12 +1,39 @@
 from github import Github
 
+print("Enter your username: ")
 username = input()
+
+print("Enter your password: ")
 password = input()
 
 g = Github(username, password)
 
+print("Logged into " + g.get_user().name)
+
+print("There are following repositories: ")
 for repo in g.get_user().get_repos():
     print(repo.name)
-    repo.edit(has_wiki=False)
-    # to see all the available attributes and methods
-    print(dir(repo))
+
+while True:
+    print("Choose one of the repositories or type 'exit'")
+    input = input()
+    if ( input == "exit"):
+        print("Goodbye")
+        break
+
+    found = False
+    for repo in g.get_user().get_repos():
+        if ( input == repo.name ):
+            found = True
+            print()
+            print("Showing all commit dates and authors:")
+            for commit in repo.get_commits():
+                print(commit.commit.author.name)
+                print(commit.commit.author.date)
+            print()
+            print("Showing all contributors:")
+            for contributor in repo.get_contributors():
+                print(contributor.name)
+
+    if ( found == False ):
+        print("That repository doesn't exist, try again!")
