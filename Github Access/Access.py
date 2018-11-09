3b565b5294551581f09666ebf8b7fc46f8fd301c
Tin Oreskovic <34209614+Tin97@users.noreply.github.com>
2018-11-09 17:41:18 +0000

    added user input

--- Github Access/Access.py
+++ Github Access/Access.py
@@ -1,6 +1,12 @@
 from github import Github
 
-g = Github("Tin97", "23081997pc")
+username = input()
+password = input()
+
+g = Github(username, password)
 
 for repo in g.get_user().get_repos():
     print(repo.name)
+    repo.edit(has_wiki=False)
+    # to see all the available attributes and methods
+    print(dir(repo))
