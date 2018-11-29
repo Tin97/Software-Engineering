ac17a593d18c851d70688d5e74674d2d905c8a1e
Tin Oreskovic <34209614+Tin97@users.noreply.github.com>
2018-11-29 15:17:43 +0000

    added commits per person functionality in a repo

--- Github Visualisation/visualization.py
+++ Github Visualisation/visualization.py
@@ -8,23 +8,34 @@ g = Github()
 print("You chose ", username)
 
 print("There are following repositories: ")
-for repo in g.get_user().get_repos(username):
+for repo in g.get_user(username).get_repos():
     print(repo.name)
 
-while True:
-    print("Choose one of the repositories or type 'exit'")
-    input = input()
-    if ( input == "exit"):
-        print("Goodbye")
-        break
 
-    found = False
-    for repo in g.get_user().get_repos():
-        if ( input == repo.name ):
-            found = True
-            
-            for contributor in repo.get_contributors():
-                print(contributor.name)
+print("Choose one of the repositories or type 'exit'")
+input = "Group30_app"
 
-    if ( found == False ):
-        print("That repository doesn't exist, try again!")
+contributors = []
+commits = []
+
+found = False
+for repo in g.get_user(username).get_repos():
+    if ( input == repo.name ):
+        found = True
+
+        for contributor in repo.get_contributors():
+            print(contributor.name)
+            contributors.append(contributor.name)
+            commits.append(0)
+
+        for commit in repo.get_commits():
+            author = commit.commit.author.name
+            for i in range(len(contributors)):
+                if author == contributors[i]:
+                    commits[i]+=1
+
+if ( found == False ):
+    print("That repository doesn't exist, try again!")
+
+for i in range(len(commits)):
+    print(contributors[i],commits[i])
